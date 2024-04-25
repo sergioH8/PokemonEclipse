ab19c680c31473f9e247b91d97ffe1ded2bdcd1d
GASergio1995 <sergio_pixi_@hotmail.com>
2024-04-25 12:26:35 +0200

    Modifico Modelo
    Modifico Enum tipo y la clase Pokemon Crud para que no de error con nuestros atributos de entrenador y que este todo correctamente escrito como lo tenemos declarado

--- src/crud/PokemonCRUD.java
+++ src/crud/PokemonCRUD.java
@@ -1,39 +1,69 @@
 package crud;
 
+import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.LinkedList;
 
+import modelo.Entrenador;
 import modelo.Pokemon;
+import modelo.Tipo;
 
 public class PokemonCRUD {
 
-	public static void obtenerPokemon(Conexion conexion, Entrenador ent, Equipo equipo) {
+	public static void obtenerPokemon(Connection conexion, Entrenador entrenador, int caja) throws SQLException {
+		String sql = "SELECT 	PK.ID_POKEMON,\r\n"
+				+ "		PX.NUM_POKEDEX,\r\n"
+				+ "		PX.NOM_POKEMON,\r\n"
+				+ "		PX.TIPO1,\r\n"
+				+ "		PX.TIPO2,\r\n"
+				+ "		PX.IMAGEN,\r\n"
+				+ "		PX.SONIDO,\r\n"
+				+ "		PX.NIVEL_EVOLUCION,\r\n"
+				+ "		PX.NUM_POKEDEX_EVO,\r\n"
+				+ "		PK.ATAQUE,\r\n"
+				+ "		PK.ATA_ESPECIAL,\r\n"
+				+ "		PK.DEFENSA,\r\n"
+				+ "		PK.MOTE,\r\n"
+				+ "		PK.NIVEL\r\n"
+				+ "FROM POKEMON PK\r\n"
+				+ "INNER JOIN POKEDEX PX\r\n"
+				+ "	ON PX.NUM_POKEDEX = PK.NUM_POKEDEX\r\n"
+				+ "WHERE PK.ID_ENTRENADOR =?\r\n"
+				+ "AND PK.CAJA =?";	
 		
 		PreparedStatement ps = conexion.prepareStatement(sql);
 		ps.setInt(1, entrenador.getIdEntrenador());
 		ps.setInt(2, caja);
 		
-		ResultSet rs = ps.PokemonQuery();
+		ResultSet rs = ps.executeQuery();
 		LinkedList<Pokemon> listadoPokemon = new LinkedList<Pokemon>();
 		Pokemon poke;
-		while(rs.next()) {
+		while (rs.next()) {
 			poke = new Pokemon();
 			poke.setIdPokemon(rs.getInt(1));
+			poke.setNumPokedex(rs.getInt(2));
 			poke.setNombre(rs.getString(3));
-			//Poner todos los atributos de la sentencia sql que se necesitan
+			poke.setTipoPrimario(Tipo.convertirTipoDesdeString(rs.getString(4)));
+			poke.setTipoSecundario(Tipo.convertirTipoDesdeString(rs.getString(5)));
+			poke.setImgFrontal(rs.getString(6));
+			poke.setNivelEvolucion(rs.getInt(8));
+			poke.setAtaque(rs.getInt(10));
+			poke.setAtaqueEspecial(rs.getInt(11));
+			poke.setDefensa(rs.getInt(12));
+			poke.setMote(rs.getString(13));
+			poke.setNivel(rs.getInt(14));
+			//poke.setMovimientos(obtenerMovimientos(conexion, poke.getIdPokemon()));
+			//poke.setObjeto(obtenerObjeto(conexion, poke.getIdPokemon()));
 			listadoPokemon.add(poke);
-
-			
 		}
 		
-		if(caja==1) {
-			entrenador.setEquipoPrincioal(listadoPokemon);
+		if(caja ==1) {
+			entrenador.setEquipoPokemon(listadoPokemon);
 		}else {
-			entrenador.setPokemonCaja(istadoPokemon);
+			entrenador.setCajaPokemon(listadoPokemon);
 		}
+		
 	}
-	
-	
-	
 }
--- src/modelo/Tipo.java
+++ src/modelo/Tipo.java
@@ -2,24 +2,45 @@ package modelo;
 
 public enum Tipo {
 
-    AGUA,       // Ordinal nos indica el orden en este caso es:   0
-    BICHO,      // Ordinal nos indica el orden en este caso es:   1
-    DRAGON,     // Ordinal nos indica el orden en este caso es:   2
-    ELECTRICO,  // Ordinal nos indica el orden en este caso es:   3
-    FANTASMA,   // Ordinal nos indica el orden en este caso es:   4
-    FUEGO,      // Ordinal nos indica el orden en este caso es:   5
-    HIELO,      // Ordinal nos indica el orden en este caso es:   6
-    LUCHA,      // Ordinal nos indica el orden en este caso es:   7
-    NORMAL,     // Ordinal nos indica el orden en este caso es:   8
-    PLANTA,     // Ordinal nos indica el orden en este caso es:   9
-    PSIQUICO,   // Ordinal nos indica el orden en este caso es:  10
-    ROCA,       // Ordinal nos indica el orden en este caso es:  11
-    TIERRA,     // Ordinal nos indica el orden en este caso es:  12
-    VENENO,     // Ordinal nos indica el orden en este caso es:  13
-    VOLADOR;    // Ordinal nos indica el orden en este caso es:  14
+    AGUA("Agua"),       	 // Ordinal nos indica el orden en este caso es:   0
+    BICHO("Bicho"),     	 // Ordinal nos indica el orden en este caso es:   1
+    DRAGON("Dragón"),    	 // Ordinal nos indica el orden en este caso es:   2
+    ELECTRICO("Eléctrico"),  // Ordinal nos indica el orden en este caso es:   3
+    FANTASMA("Fantasma"),    // Ordinal nos indica el orden en este caso es:   4
+    FUEGO("Fuego"),      	 // Ordinal nos indica el orden en este caso es:   5
+    HIELO("Hielo"),     	 // Ordinal nos indica el orden en este caso es:   6
+    LUCHA("Lucha"),     	 // Ordinal nos indica el orden en este caso es:   7
+    NORMAL("Normal"),    	 // Ordinal nos indica el orden en este caso es:   8
+    PLANTA("Planta"),    	 // Ordinal nos indica el orden en este caso es:   9
+    PSIQUICO("Psíquico"),    // Ordinal nos indica el orden en este caso es:  10
+    ROCA("Roca"),      		 // Ordinal nos indica el orden en este caso es:  11
+    TIERRA("Tierra"),    	 // Ordinal nos indica el orden en este caso es:  12
+    VENENO("Veneno"),    	 // Ordinal nos indica el orden en este caso es:  13
+    VOLADOR("Volador");   	 // Ordinal nos indica el orden en este caso es:  14
+	
+	
+private final String nombre;
+	
 
 
-        static final double[][] multiplicadores = {
+
+
+        public String getNombre() {
+	return nombre;
+}
+
+
+public static double[][] getMultiplicadores() {
+	return multiplicadores;
+}
+
+
+		private Tipo(String nombre) {
+		this.nombre = nombre;
+	}
+
+
+		static final double[][] multiplicadores = {
                       //AGUA    BICHO   DRAGON  ELECTRICO FANTASMA FUEGO   HIELO   LUCHA   NORMAL  PLANTA  PSIQUICO  ROCA    TIERRA  VENENO  VOLADOR
         /* AGUA*/      {0.5,    1,      0.5,    1,        1,       2,      1,      1,      1,      0.5,    1,        2,      2,      1,      1  },    // AGUA
         /* BICHO*/     {1,      1,      1,      1,        0.5,     0.5,    1,      0.5,    1,      2,      1,        1,      1,      2,      0.5},    // BICHO
@@ -38,5 +59,16 @@ public enum Tipo {
         /* VOLADOR */  {1,      2,      1,      1,        1,       1,      0.5,    2,      1,      2,      1,        0.5,    2,      1,      1  }     // VOLADOR
         };
 
+        
+     // Método estático para convertir una cadena a un valor del enumerado
+    	public static Tipo convertirTipoDesdeString(String tipoString){ 
+    		for (Tipo tipo : Tipo.values()) { 
+    			if(tipo.getNombre().toUpperCase().equalsIgnoreCase(tipoString)) { 
+    				return tipo; 
+    			} 
+    		} 
+    		return null; 
+    	}
+        
     }
 
