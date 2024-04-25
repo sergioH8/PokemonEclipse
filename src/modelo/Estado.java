9f7e5b2fde1febeaa6904120a4481b7c3e438b72
GASergio1995 <sergio_pixi_@hotmail.com>
2024-04-25 13:03:53 +0200

    Modifico Enum Estado

--- src/modelo/Estado.java
+++ src/modelo/Estado.java
@@ -1,24 +1,36 @@
 package modelo;
 
 public enum Estado {
-    PARALIZADO,                 // Ordinal nos indica el orden en este caso es:   0
-    QUEMADO,                    // Ordinal nos indica el orden en este caso es:   1
-    ENVENENADO,                  // Ordinal nos indica el orden en este caso es:   2
-    GRAVEMENTE_ENVENENADO,       // Ordinal nos indica el orden en este caso es:   3
-    DORMIDO,                     // Ordinal nos indica el orden en este caso es:   4
-    CONGELADO,                         // Ordinal nos indica el orden en este caso es:   5
-    HELADO,                            // Ordinal nos indica el orden en este caso es:   6
-    SOMNOLIENTO,                       // Ordinal nos indica el orden en este caso es:   7
-    POKERUS,                   // Ordinal nos indica el orden en este caso es:   8
-    DEBILITADO,                   // Ordinal nos indica el orden en este caso es:   9
-    CONFUSO,                   // Ordinal nos indica el orden en este caso es:  10
-    ENAMORADO,                // Ordinal nos indica el orden en este caso es:  11
-    ATRAPADO,                  // Ordinal nos indica el orden en este caso es:  12
-    MALDITO,                      // Ordinal nos indica el orden en este caso es:  13
-    DRENADORAS,                       // Ordinal nos indica el orden en este caso es:  14
-    CANTO_MORTAL,
-    CENTRO_DE_ATENCION,
-    AMEDRENTADO;
+	
+	
+	PARALIZADO("Paralizado"),                		 // Ordinal nos indica el orden en este caso es:   0
+    QUEMADO("Quemado"),                    			 // Ordinal nos indica el orden en este caso es:   1
+    ENVENENADO("Envenenado"),                  		 // Ordinal nos indica el orden en este caso es:   2
+    GRAVEMENTE_ENVENENADO("Gravemente_envenenado"),  // Ordinal nos indica el orden en este caso es:   3
+    DORMIDO("Dormido"),                     		 // Ordinal nos indica el orden en este caso es:   4
+    CONGELADO("Congelado"),                          // Ordinal nos indica el orden en este caso es:   5
+    HELADO("Helado"),                            	 // Ordinal nos indica el orden en este caso es:   6
+    SOMNOLIENTO("Somnoliento"),                      // Ordinal nos indica el orden en este caso es:   7
+    POKERUS("Pokerus"),                   			 // Ordinal nos indica el orden en este caso es:   8
+    DEBILITADO("Debilitado"),                   	 // Ordinal nos indica el orden en este caso es:   9
+    CONFUSO("Confuso"),                   			 // Ordinal nos indica el orden en este caso es:  10
+    ENAMORADO("Enamorado"),                			 // Ordinal nos indica el orden en este caso es:  11
+    ATRAPADO("Atrapado"),                            // Ordinal nos indica el orden en este caso es:  12
+    MALDITO("Maldito"),                      		 // Ordinal nos indica el orden en este caso es:  13
+    DRENADORAS("Drenadoras"),                        // Ordinal nos indica el orden en este caso es:  14
+    CANTO_MORTAL("Canto_mortal"),
+    CENTRO_DE_ATENCION("Crentro_de_atencion"),
+    AMEDRENTADO("Amedrentado");
+
+    private final String nombre;
+
+    Estado(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
 
     public boolean esPersistente() {
         switch (this) {
